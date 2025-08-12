48bfe971b8328e70ba13299f9e68a236929421af
khalafyangagik <khalafyangagik69@gmail.com>
2025-08-12 22:07:55 +0400

    Create MaximumSubArray.cs

---
+++ Arrays/MaximumSubArray.cs
@@ -0,0 +1,15 @@
+public class Solution 
+{
+    public int MaxSubArray(int[] nums) 
+    {
+        int currentSum = nums[0];
+        int bestSum = nums[0];
+
+        for(int i = 1; i < nums.Length; ++i)
+        {
+            currentSum = Math.Max(nums[i],currentSum + nums[i]);
+            bestSum = Math.Max(currentSum,bestSum);
+        }    
+        return bestSum;
+    }
+}
