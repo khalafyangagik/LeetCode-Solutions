903174a9e3de2a6ee06e42d533f92c098baada1d
khalafyangagik <khalafyangagik69@gmail.com>
2025-08-12 22:23:05 +0400

    Create Search2DMatrix2.cs

---
+++ Arrays/Search2DMatrix2.cs
@@ -0,0 +1,31 @@
+public class Solution 
+{
+    public bool SearchMatrix(int[][] matrix, int target) 
+    {
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+
+        int row = 0;
+        int col = n - 1;
+
+        while(row < m && col >= 0)
+        {
+            int val = matrix[row][col];
+
+            if(val == target)
+            {
+                return true;
+            }
+
+            else if(val > target)
+            {
+                col--;
+            }
+            else 
+            {
+                row++;
+            }
+        }
+        return false;
+    }
+}
